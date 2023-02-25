3f4fc283cb9d4f73dad84ffaeff4f87468a004af
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2023-02-25 16:12:32 +0000

    json file, still issue with post method

--- client/client.js
+++ client/client.js
@@ -1,29 +1,50 @@
-//window.addEventListener('click', function(event){
-    //fetch('http://127.0.0.1:8090/list')
-    //fetch('http://127.0.0.1:8090/lost')
-    //fetch('http://1277.0.0.1:8090/list')
-    //fetch('http://126.0.0.1:8090/list')
+// window.addEventListener('click', function(event){
+//   fetch('http://127.0.0.1:8090/list')
+//   .then(response => response.text())
+//   .then(body =>document.getElementById('content').innerHTML=body)
 
+// })
 
-    //  .then(response => response.text())
-    //  .then(body =>
-    //     document.getElementById('content').innerHTML=body)
-    //     .catch((error) => alert(error))
+document.getElementById('submitBtn').addEventListener('click', function(event){
+  fetch('http://127.0.0.1:8090/thing/list')
+   .then(response => response.json())
+   .then(body => renderThings(body))
+   //.then(body => renderThings(JSON.parse(body)))
+    //  document.getElementById('things').innerHTML=body)
+      .catch((error) => alert(error))
+});
 
-    window.addEventListener('click', async function(event){
-        let response = await fetch('http://127.0.0.1:8090/list');
-        let body = await response.text();
-        document.getElementById('content').innerHTML=body
-      });
- // });
+function renderThings (animalFacts){
+  //alert(things);
+  let container = document.getElementById('animals');
+  for(let thing of animalFacts){
+     let item = document.createElement('li')
+     item.innerHTML = thing;
+     container.appendChild(item);
+  }
+  };
 
+  var animals = require('./animals.json');
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.urlencoded({extended: false}));
 
-//  window.addEventListener('click', async function(event){
-//     try{
-//       let response = await fetch('http://127.0.0.1:8090/list');
-//       let body = await response.text();
-//       document.getElementById('content').innerHTML=body;
-//     } catch(e) {
-//       alert(e);
-//     }
-// });
+  app.post('/new', function(req,resp){
+    console.log('Got request');
+    console.log(req.body);
+    const name = req.body.name;
+    const breed = req.body.breed;
+    const description = req.body.description;
+    const age = req.body.age;
+    const country = req.body.country;
+
+    const animal = {
+        'name': name,
+        'breed': breed,
+        'description': description,
+        'age': age,
+        'country': country,
+    };
+    animals.push(animal);
+    resp.send('Thank you for submitting an animal')
+})
+
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-app.use(express.static(`client`));
 
-// let instruments = [ 'Grand piano', 'Upright Piano', 'Electric Piano','Hybrid Piano'];
+app.use(express.static('client'));
 
-app.get('/list', function (req, resp){
-    resp.send(instruments);
-});
+let animalFacts = ['Our animals come from all over the world',
+    'They quite often come from harsh backgrounds and require extra care and support from us',
+  'We can keep looking after them with your help',
+'Donate today!!'];
 
-app.listen(8090);
+app.get('/thing/list', function(req, resp){
+    // resp.send(things);
+    resp.json(animalFacts);
+   })
+
+app.get("/new", (request, response) => {
+    response.send("index.html");
+    });
+
+app.listen(8090)
