4d988e0422fa5fed1e5cdf9b3ec8b25ad0ddfffa
AmyBarnhouse <107323584+AmyBarnhouse@users.noreply.github.com>
2023-03-14 21:44:41 +0000

    animal desciptions

--- client/client.js
+++ client/client.js
@@ -1,33 +1,29 @@
-// prints list of facts about sponsorship of the farmyard animals
-document.getElementById('submitBtn').addEventListener('click', (event) => {
-  fetch('http://127.0.0.1:8090/thing/list')
-    .then((response) => response.json())
-    .then((body) => renderThings(body))
-  // .then(body => renderThings(JSON.parse(body)))
-    //  document.getElementById('things').innerHTML=body)
-    .catch((error) => alert(error));
-});
-
-document.getElementById('submitBtn').onclick = function () {
-  // disable
-  this.disabled = true;
-
-  // do some validation stuff
-};
-
 function renderThings(animalFacts) {
   // alert(things);
   const container = document.getElementById('animalFacts');
-  for (const thing of animalFacts) {
-    const item = document.createElement('li');
-    item.innerHTML = thing;
-    container.appendChild(item);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const animalFact of animalFacts) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = animalFact;
+    container.appendChild(listItem);
   }
 }
 
-// snackbar displays pop-up message when the user signs up 
-//to provide a positive experience for them and show gratitude for sponsoring the animals.
-document.getElementById('submitForm').addEventListener('click', (event) => {
+// prints list of facts about sponsorship of the farmyard animals
+document.getElementById('submitBtn').addEventListener('click', () => {
+// checks to see if animalFacts ul is empty
+// if so, fetch list of animalFacts and populate using renderThings function
+  if (document.getElementById('animalFacts').getElementsByTagName('li').length === 0) {
+    fetch('http://127.0.0.1:8090/thing/list')
+      .then((response) => response.json())
+      .then((body) => renderThings(body))
+      .catch((error) => alert(error));
+  }
+});
+
+// snackbar displays pop-up message when the user signs up
+// to provide a positive experience for them and show gratitude for sponsoring the animals.
+document.getElementById('submitForm').addEventListener('click', () => {
 /* find the div element where the snackbar will be inserted */
   const x = document.getElementById('snackbar');
   x.className = 'show';
@@ -36,30 +32,35 @@ document.getElementById('submitForm').addEventListener('click', (event) => {
 });
 let id = null;
 /** This function produces an animation.
-   * I have a cartoon image of a sun moving a small section along the page 
+   * I have a cartoon image of a sun moving a small section along the page
    * in response to the user pressing submit to subscibe to sanctuary's news.
    */
 
-document.getElementById('submitForm').addEventListener('click', (event) => {
-  const elem = document.getElementById('sun');
-  let pos = 0;
-  console.log(window.innerWidth);
-  clearInterval(id);
-  id = setInterval(frame, 2);
+document.getElementById('submitForm').addEventListener('click', () => {
+  const sunImage = document.getElementById('sun');
+  let position = 0;
+  // console.log(window.innerWidth);
+  // clearInterval(id);
+
   function frame() {
-    if (pos == 200) {
+    if (position === 200) {
       clearInterval(id);
     } else {
-      pos++;
+      // eslint-disable-next-line no-plusplus
+      position++;
       // elem.style.top = pos + 'px';
-      elem.style.left = `${pos}px`;
+      sunImage.style.left = `${position}px`;
     }
   }
+
+  id = setInterval(frame, 2);
 });
 
+// eslint-disable-next-line no-use-before-define
 document.getElementById('emailMsg').addEventListener('click', emailAlert);
 /** Displays alert to user when they click to put in their email address. */
 function emailAlert() {
+  // eslint-disable-next-line no-alert
   alert('We promise to only send you helpful and relevant information.');
 }
 
@@ -84,35 +85,64 @@ function emailAlert() {
 //   });
 
 
-document.getElementById('about0').addEventListener('click', (event) => {
-  const box = document.createElement('p')
-  box.setAttribute('id', 'boxAbout0')
-})
+// document.getElementById('about0').addEventListener('click', (event) => {
+//   const box = document.createElement('p')
+//   box.setAttribute('id', 'boxAbout0')
+// })
+
+// fetch('animals.json')
+// .then((response) => response.json())
+// .then(data => {
+//   document.querySelector('#aniName0').innerText = data[0].name;
+//   document.querySelector('#aniBreed0').innerText = data[0].breed;
+//   document.querySelector('#aniName1').innerText = data[1].name;
+//   document.querySelector('#aniBreed1').innerText = data[1].breed;
+//   document.querySelector('#aniName2').innerText = data[2].name;
+//   document.querySelector('#aniBreed2').innerText = data[2].breed;
+//   document.querySelector('#aniName3').innerText = data[3].name;
+//   document.querySelector('#aniBreed3').innerText = data[3].breed;
+//   document.querySelector('#aniName4').innerText = data[4].name;
+//   document.querySelector('#aniBreed4').innerText = data[4].breed;
+//   document.querySelector('#aniName5').innerText = data[5].name;
+//   document.querySelector('#aniBreed5').innerText = data[5].breed;
+//   document.querySelector('#aniName6').innerText = data[6].name;
+//   document.querySelector('#aniBreed6').innerText = data[6].breed;
+//   document.querySelector('#aniName7').innerText = data[7].name;
+//   document.querySelector('#aniBreed7').innerText = data[7].breed;
+//   document.querySelector('#aniName8').innerText = data[8].name;
+//   document.querySelector('#aniBreed8').innerText = data[8].breed;
+
+// })
+
+fetch('animals.json')
+  .then((response) => response.json())
+  .then((animals) => {
+    animals.forEach((animal, index) => {
+      console.log(animal);
+      console.log(index);
+
+      console.log('aniName'+index);
+      console.log(animal.name);
+
+      document.getElementById('aniName' + index).innerText = animal.name;
+      document.getElementById('aniBreed' + index).innerText = animal.breed;
+      document.getElementById('aniDesc' + index).innerText = animal.description;
 
 
-fetch("animals.json")
-.then(response => response.json())
-.then(data => {
-  document.querySelector('#aniName0').innerText = data[0].name
-  document.querySelector('#aniBreed0').innerText = data[0].breed
-  document.querySelector('#aniName1').innerText = data[1].name
-  document.querySelector('#aniBreed1').innerText = data[1].breed
-  document.querySelector('#aniName2').innerText = data[2].name
-  document.querySelector('#aniBreed2').innerText = data[2].breed
-  document.querySelector('#aniName3').innerText = data[3].name
-  document.querySelector('#aniBreed3').innerText = data[3].breed
-  document.querySelector('#aniName4').innerText = data[4].name
-  document.querySelector('#aniBreed4').innerText = data[4].breed
-  document.querySelector('#aniName5').innerText = data[5].name
-  document.querySelector('#aniBreed5').innerText = data[5].breed
-  document.querySelector('#aniName6').innerText = data[6].name
-  document.querySelector('#aniBreed6').innerText = data[6].breed
-  document.querySelector('#aniName7').innerText = data[7].name
-  document.querySelector('#aniBreed7').innerText = data[7].breed
-  document.querySelector('#aniName8').innerText = data[8].name
-  document.querySelector('#aniBreed8').innerText = data[8].breed
+      document.getElementById('detailButton' + index).addEventListener('click', () => {
 
-})
+        
+        let x = document.getElementById('aniDesc' + index);
+        if (x.style.display === 'none') {
+          x.style.display = 'block';
+        } else {
+          x.style.display = 'none';
+        }
+      });
+    });
+  });
+
+
 
 
 // document.getElementById('volunteers').addEventListener('click', (event) => {
@@ -141,3 +171,18 @@ function renderVols(volFarms){
     contain.appendChild(item);
   }
 }
+
+function dropdown(){
+fetch('/animal')
+.then((response) => response.json())
+.then((data) => {
+console.log(data)
+  data.forEach(animal) 
+  const adopts = document.querySelector('select#aanimals');
+    console.log(adopts)
+    const opt = document.createElement('option');
+    opt.label = data.name;
+    opt.value = data.name;
+    adopts.appendChild(opt); 
+  }
+)}
